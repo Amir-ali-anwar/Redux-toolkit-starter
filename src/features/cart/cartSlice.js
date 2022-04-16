8996f1307f237aa61f33c309f89111dfe2a9feb9
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-04-16 18:15:03 +0500

    cart total completed

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -6,7 +6,6 @@ const initialState={
     amount:0,
     total:0
 }
-console.log(initialState)
 const cartSlice=createSlice({
     name:'cart',
     initialState,
@@ -21,25 +20,24 @@ const cartSlice=createSlice({
         increasecart:(state,action)=>{
         const Itemamount= state.cart.find((item)=>item.id ===action.payload)
         Itemamount.amount = Itemamount.amount + 1;
-        // console.log(Itemamount)   
         },
         decreasecart:(state,action)=>{
             const id=action.payload;
             const Itemamount= state.cart.find((item)=>item.id ===id)
             Itemamount.amount = Itemamount.amount - 1; 
-            // console.log(Itemamount)
         },
         calculateTotals:(state)=>    {
-            let { amount, total }= state.cart.reduce((acc,curr)=>{
+        state.cart.reduce((acc,curr)=>{
                 acc.amount+=curr.amount;
-                acc.total=curr.amount*curr.price
-                console.log({
-                    amount:acc.amount,
-                    total:acc.total
-                })
+                acc.total+=curr.amount*curr.price
+                {
+                  state.amount=acc.amount
+                  state.total=acc.total  
+                }           
                return acc; 
-            },{amount:state.amount,total:state.total})
-        }
+            },{amount:0,total:0})
+        },
+        
     }
 })
 export const {clearcart,removeItem,increasecart,decreasecart,calculateTotals} = cartSlice.actions
